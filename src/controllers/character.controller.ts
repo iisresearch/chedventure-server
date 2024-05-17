bf44212cb8e07c4dfaec004e45005f2483287297
Kiril <kirilbuga@yahoo.com>
2024-05-17 10:13:10 +0200

    Add documentation and revise createCharacter method in CharacterController
    The CharacterController class has been modified for for improved code readability and understanding. Documentation has been added for class and methods. The method to create a new character has been refactored to separate concerns by creating a new helper method, fillDefaultContexts, to populate default contexts.

--- src/controllers/character.controller.ts
+++ src/controllers/character.controller.ts
@@ -1,20 +1,30 @@
-import {
-    deleteCharacter,
-    getCharacter,
-    getCharactersToGame, saveCharacter
-} from "../repositories/character";
+import {deleteCharacter, getCharacter, getCharactersToGame, saveCharacter} from "../repositories/character";
 import {Character} from "../models/character";
 import {getGame} from "../repositories/game";
 import {DeleteResult} from "typeorm";
 import {Context} from "../models/context";
-import {saveContext} from "../repositories/context";
 
+/**
+ * CharacterController class handles all operations related to Characters.
+ */
 export default class CharacterController {
 
+    /**
+     * Retrieves the characters associated with a specified game.
+     * @param {string} gameId - The ID of the game.
+     * @return {Promise<Array<Character>>} - A Promise that resolves to an array of Character objects.
+     */
     public async getCharactersInGame(gameId: string): Promise<Array<Character>> {
         return getCharactersToGame(gameId);
     }
 
+    /**
+     * Creates a new character for a specific game and user.
+     * @param {string} gameId - The ID of the game.
+     * @param {string} userId - The ID of the user.
+     * @param {Object} req - The request object containing the character details.
+     * @returns {Promise<Character | null | undefined>} A Promise that resolves with the created character, or null if the game is not found, or undefined if an error occurred.
+     */
     public async createCharacter(gameId: string, userId: string, req: any): Promise<Character | null | undefined> {
         const g = getGame(gameId, userId);
 
@@ -28,26 +38,8 @@ export default class CharacterController {
             character.history = req.history;
             character.chatbotUrl = req.chatbotUrl;
             character.contexts = [];
-            // TODO:
-            // character.context add Start Context to each new character by default
-            // createContext();
 
-            // Create default contexts for the new character
-            const contextNames = ["Start", "Option", "End"];
-
-            //Promise.resolve(contextNames).then((name)=> {
-
-            for (let name of contextNames) {
-                const context = new Context();
-                context.character = character;
-                context.name = name;
-                context.game = character.game;
-                context.messages = [];
-                //let savedContext = saveContext(context);
-                // if (savedContext!=null)
-                character.contexts.push(context)
-            }
-            //});
+            CharacterController.fillDefaultContexts(character);
 
             return saveCharacter(character);
 
@@ -55,7 +47,30 @@ export default class CharacterController {
             return e;
         });
     }
+    /**
+     * Fills the default contexts for a new character.
+     * @param {Character} character - The character to fill the contexts for.
+     */
+    private static fillDefaultContexts(character: Character) {
+        // Create default contexts for the new character
+        const contextNames = ["Start", "Option", "End"];
 
+        for (let name of contextNames) {
+            const context = new Context();
+            context.character = character;
+            context.name = name;
+            context.game = character.game;
+            context.messages = [];
+            character.contexts.push(context)
+        }
+    }
+
+    /**
+     * Updates a character.
+     * @param {number} id - The ID of the character to update.
+     * @param {any} req - The request object containing the new character details.
+     * @returns {Promise<Character | null | undefined>} - A promise that resolves to the updated Character or null.
+     */
     public async updateCharacter(id: number, req: any): Promise<Character | null | undefined> {
         const ch = getCharacter(id);
 
@@ -73,6 +88,11 @@ export default class CharacterController {
         });
     }
 
+    /**
+     * Deletes a character.
+     * @param {number} id - The ID of the character to delete.
+     * @returns {Promise<DeleteResult>} - A promise that resolves to the result of the delete operation.
+     */
     public async deleteCharacter(id: number): Promise<DeleteResult> {
         return deleteCharacter(id);
     }
