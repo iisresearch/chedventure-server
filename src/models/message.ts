fdc7b384d51475ecefad67b027d4e05e94ff0318
Kiril <kirilbuga@yahoo.com>
2024-07-05 10:18:04 +0200

    Update for continuation type: string, added fallback, variable...

--- src/models/message.ts
+++ src/models/message.ts
@@ -4,24 +4,34 @@ import {
     PrimaryGeneratedColumn, ManyToOne
 } from "typeorm";
 import {Context} from "./context";
+import {Character} from "./character";
 
 @Entity()
 export class Message {
     @PrimaryGeneratedColumn()
     intent!: number;
 
-    @Column({nullable: true})
-    continuation!: number;
+    @Column({default: ''})
+    continuation!: string;
 
-    @Column({nullable: true})
+    @Column({default: ''})
     contextualisation!: string;
 
-    @Column()
+    @Column({default: ''})
     utterance!: string;
 
-    @Column()
+    @Column({default: ''})
     response!: string;
+
+    @Column({default: ''})
+    fallback!: string;
+
+    @Column({default: ''})
+    variable!: string;
 
     @ManyToOne(() => Context, (context) => context.messages, {onDelete: "CASCADE"})
     context!: Context;
+
+    @ManyToOne(() => Character, (character) => character.messages, {onDelete: "CASCADE"})
+    character!: Character;
 }
