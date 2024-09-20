96940a694c07c0c00c04f066c77012904f56a1c3
Kiril <kirilbuga@yahoo.com>
2024-09-20 13:49:59 +0200

    on new Context add multiple messages

--- src/repositories/context.ts
+++ src/repositories/context.ts
@@ -2,6 +2,8 @@ import {Context} from "../models/context";
 import {AppDataSource} from "../data-source";
 import {DeleteResult} from "typeorm";
 import {Room} from "../models/room";
+import {Message} from "../models/message";
+import {saveMessages} from "./message";
 
 export const getContext = async (id: number): Promise<Context | null> => {
     const contextRepository = await AppDataSource
@@ -50,6 +52,22 @@ export const deleteContext = async (id: number): Promise<DeleteResult> => {
 
 export const saveContext = async (context: Context): Promise<Context | null> => {
     const contextRepository = AppDataSource.getRepository(Context);
+    // Ensure each message is properly instantiated and assigned the correct values
+    context.messages = context.messages.map((messageData) => {
+        const message = new Message();
+        message.intent = messageData.intent;
+        message.continuation = messageData.continuation;
+        message.contextualisation = messageData.contextualisation;
+        message.utterance = messageData.utterance;
+        message.response = messageData.response;
+        message.fallback = messageData.fallback;
+        message.variable = messageData.variable;
+        message.context = context;
+        message.character = context.character;
+        return message;
+    });
+    const messages = await saveMessages(context.messages);
+
     const co = await contextRepository.save(context)
     return await getContext(co.id);
 }
