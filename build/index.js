7128797f3e87fc3a9091fc69bb3b4a2faa6dabb5
Kiril <kirilbuga@yahoo.com>
2024-10-25 16:45:04 +0200

    For DB update

--- build/index.js
+++ build/index.js
@@ -4,32 +4,33 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 require("reflect-metadata");
-const typeorm_1 = require("typeorm");
 const express_1 = __importDefault(require("express"));
-const auth_1 = require("./auth");
 const morgan_1 = __importDefault(require("morgan"));
 const routes_1 = __importDefault(require("./routes"));
-const database_1 = __importDefault(require("./config/database"));
+const data_source_1 = require("./data-source");
 const PORT = process.env.PORT || 4201;
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const bearerToken = require('express-bearer-token');
+const fileUpload = require('express-fileupload');
 const app = (0, express_1.default)();
 app.use(cors());
-app.use(express_1.default.json());
+//app.use(express.json());
+app.use(express_1.default.json({ limit: '5mb' }));
+app.use(express_1.default.urlencoded({ limit: '5mb', extended: true }));
 app.use((0, morgan_1.default)("tiny"));
 app.use(express_1.default.static("public"));
 app.use(bodyParser.json());
 app.use(bearerToken());
-app.use(auth_1.oktaAuth);
 app.use(routes_1.default);
-(0, typeorm_1.createConnection)(database_1.default)
-    .then((_connection) => {
+app.use(fileUpload);
+data_source_1.AppDataSource.initialize()
+    .then(() => {
+    console.log("Data Source has been initialized!");
     app.listen(PORT, () => {
         console.log("Server is running on port", PORT);
     });
 })
     .catch((err) => {
-    console.log("Unable to connect to db", err);
-    process.exit(1);
+    console.error("Error during Data Source initialization", err);
 });
--- src/data-source.ts
+++ src/data-source.ts
@@ -11,17 +11,20 @@ import {Context} from "./models/context";
 import {Message} from "./models/message";
 import {ContextSubscriber} from "./models/subscribers/contextSubscriber";
 import {MessageSubscriber} from "./models/subscribers/messageSubscriber";
+import dotenv from 'dotenv';
 
 require('dotenv').config();
+dotenv.config({ path: 'src/.env' });
+console.log("DB: ", process.env.DATABASE_URL);
 
 export const AppDataSource = new DataSource({
     type: "postgres",
     url: process.env.DATABASE_URL,
-    /*host: process.env.POSTGRES_HOST || "localhost",
-    port: Number(process.env.POSTGRES_PORT) || 5432,
-    username: process.env.POSTGRES_USER || "test",
-    password: process.env.POSTGRES_PASSWORD || "test",
-    database: process.env.POSTGRES_DB || "test",*/
+    // host: process.env.POSTGRES_HOST || "localhost",
+    // port: Number(process.env.POSTGRES_PORT) || 5432,
+    // username: process.env.POSTGRES_USER || "test",
+    // password: process.env.POSTGRES_PASSWORD || "test",
+    // database: process.env.POSTGRES_DB || "test",
     entities: [Game, Room, Hitbox, Coordinate, Character, Context, Message, RoomToGame, HitboxToRoomToGame],
     subscribers: [ContextSubscriber, MessageSubscriber],
     migrations: ["src/migration/*.ts"],
